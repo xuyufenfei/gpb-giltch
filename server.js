f7ee8b45ba9bd4a4181ccd16752d94b2250475e4
xuyufenfei <133896005+xuyufenfei@users.noreply.github.com>
2024-06-24 14:09:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get("/test", (req, res) => {
 app.use(
     "/" + "*",
     createProxyMiddleware({
-        target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
+        target: "http://127.0.0.1:3000/", // 需要跨域处理的请求地址
         changeOrigin: false, // 默认false，是否需要改变原始主机头为目标URL
         ws: true,
         logLevel: "error",
