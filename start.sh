bcfc5e6f5f5eace76f978ad036f49fbec3afca04
xuyufenfei <133896005+xuyufenfei@users.noreply.github.com>
2024-06-24 14:03:23 +0800

    原神启动

---
+++ start.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Harry-zklcdc/go-proxy-bingai/releases/download/v2.4.3/go-proxy-bingai-linux-amd64.tar.gz
+tar -xzvf go-proxy-bingai-linux-amd64.tar.gz
+exec ./go-proxy-bingai
